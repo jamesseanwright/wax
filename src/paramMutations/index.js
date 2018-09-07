93b2f0e3a535d99e54e6cdfee505d6ea6993e63b
James Wright <james.wright@yld.io>
2018-09-07 09:53:05 +0100

    Adds additional param mutations

--- src/paramMutations/index.js
+++ src/paramMutations/index.js
@@ -3,3 +3,5 @@ import createParamMutator from './createParamMutator';
 export const setValueAtTime = createParamMutator('setValueAtTime');
 export const linearRampToValueAtTime = createParamMutator('linearRampToValueAtTime');
 export const exponentialRampToValueAtTime = createParamMutator('exponentialRampToValueAtTime');
+export const setTargetAtTime = createParamMutator('setTargetAtTime');
+export const setValueCurveAtTime = createParamMutator('setValueCurveAtTime');
