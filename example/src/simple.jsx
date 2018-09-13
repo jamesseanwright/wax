9735c63d424f0e753c8d090fbb41db967f62e282
James Wright <james.wright@yld.io>
2018-09-13 09:25:46 +0100

    Adds simple example entry point

---
+++ example/src/simple.jsx
@@ -0,0 +1,31 @@
+/** @jsx createAudioElement */
+
+import {
+    createAudioElement,
+    renderAudioGraph,
+    AudioGraph,
+    Oscillator,
+    Gain,
+    StereoPanner,
+    Destination,
+    setValueAtTime,
+    exponentialRampToValueAtTime,
+} from 'web-audio-x';
+
+renderAudioGraph(
+    <AudioGraph>
+        <Oscillator
+            frequency={[
+                setValueAtTime(200, 0),
+                exponentialRampToValueAtTime(800, 3),
+            ]}
+            type="square"
+            endTime={3}
+        />
+        <Gain gain={0.2} />
+        <StereoPanner pan={-1} />
+
+        <Destination />
+    </AudioGraph>,
+    audioContext,
+);
