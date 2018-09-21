c93a075af595af4af61a9db53bf1ae6c0c8a889e
James Wright <james@jamesswright.co.uk>
2018-09-21 17:16:55 +0100

    Adds tests for connectNodes

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    testRegex: 'src/.*/__tests__/.*\\.test\\.jsx?$',
+    testRegex: 'src\\/(.*\\/)*__tests__\\/.*\\.test\\.jsx?$',
     transform: {
         '.*\\.jsx?$': 'babel-jest',
     },
---
+++ src/__tests__/connectNodes.test.js
@@ -0,0 +1,70 @@
+import { NO_OP } from '../components/NoOp';
+import connectNodes from '../connectNodes';
+
+const createArrayWith = (length, creator) =>
+    Array(length).fill(null).map(creator);
+
+const createStubAudioNode = () => ({
+    connect: jest.fn(),
+});
+
+/* we concat item[item.length - 1] if item
+ * is an array to match the reducing nature
+ * of the connectNodes function. */
+const flatten = array => array.reduce(
+    (arr, item) => (
+        arr.concat(Array.isArray(item) ? item[item.length - 1] : item)
+    ), []);
+
+describe('connectNodes', () => {
+    it('should sequentially connect an array of audio nodes and return the last', () => {
+        const nodes = createArrayWith(5, createStubAudioNode);
+        const result = connectNodes(nodes);
+
+        expect(result).toBe(nodes[nodes.length - 1]);
+
+        nodes.reduce((previousNode, currentNode) => {
+            expect(previousNode.connect).toHaveBeenCalledTimes(1);
+            expect(previousNode.connect).toHaveBeenCalledWith(currentNode);
+            return currentNode;
+        });
+    });
+
+    it('should not connect NO_OP nodes', () => {
+        const noOpIndex = 3;
+
+        const nodes = createArrayWith(
+            5,
+            (_, i) => i === noOpIndex ? NO_OP : createStubAudioNode(),
+        );
+
+        connectNodes(nodes);
+
+        nodes.reduce((previousNode, currentNode) => {
+            if (currentNode === NO_OP) {
+                expect(previousNode.connect).not.toHaveBeenCalled();
+            } else if (previousNode !== NO_OP) {
+                expect(previousNode.connect).toHaveBeenCalledTimes(1);
+                expect(previousNode.connect).toHaveBeenCalledWith(currentNode);
+            }
+
+            return currentNode;
+        });
+    });
+
+    it('should recursively reduce multidimensional arrays of AudioNodes', () => {
+        const nodes = [
+            ...createArrayWith(3, createStubAudioNode),
+            createArrayWith(4, createStubAudioNode),
+            ...createArrayWith(2, createStubAudioNode),
+        ];
+
+        connectNodes(nodes);
+
+        flatten(nodes).reduce((previousNode, currentNode) => {
+            expect(previousNode.connect).toHaveBeenCalledTimes(1);
+            expect(previousNode.connect).toHaveBeenCalledWith(currentNode);
+            return currentNode;
+        });
+    });
+});
